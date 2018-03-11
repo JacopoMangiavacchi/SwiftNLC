2fab7c6c80f015ed9171f63644849e711aa452a9
J-Mangia <j.mangia@ibm.com>
2018-03-10 20:49:00 -0800

    Importing SwiftCLI

--- Importer/Watson/Package.swift
+++ Importer/Watson/Package.swift
@@ -7,13 +7,13 @@ let package = Package(
     name: "Watson",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Watson",
-            dependencies: []),
+            dependencies: ["SwiftCLI"]),
     ]
 )
--- Importer/Watson/Sources/Watson/main.swift
+++ Importer/Watson/Sources/Watson/main.swift
@@ -1,1 +1,19 @@
-print("Hello, world!")
+import Foundation
+import SwiftCLI
+
+class ImportCommand: Command {
+    let name = "import"
+    let shortDescription = "import an IBM Watson workspace JSON file"
+    let path = Parameter()
+    
+    func execute() throws {
+        print("Importing \(path.value)")
+
+
+
+    }
+}
+
+let importer = CLI(name: "Watson", version: "1.0.0", description: "SwiftNLC - IBM Watson Converstation workspace importer")
+importer.commands = [ImportCommand()]
+let _ = importer.go()
